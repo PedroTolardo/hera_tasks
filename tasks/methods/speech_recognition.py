aeefa2ffad27bc4bf30bd62c2a03c6c99c8d416d
khaledhazime <hazimekhaled01@gmail.com>
2023-04-19 01:18:07 -0300

    feat(speech_recognition.py): add Speech class with talk and hear methods

---
+++ tasks/methods/speech_recognition.py
@@ -0,0 +1,53 @@
+import actionlib
+import rospy
+from hera.msg import (hearFeedback, hearResult, hearAction, hearGoal,
+                      talkFeedback, talkResult, talkAction, talkGoal)
+
+
+class Speech:
+    """
+    A class that contains the speech methods of the HERA robot
+
+    Attributes
+    ----------
+    client_talk : actionlib.SimpleActionClient
+        A client that talks to the talk service
+    client_hear : actionlib.SimpleActionClient
+        A client that listens to the hear service
+
+    Methods
+    -------
+    talk(phrase: str, from_language: str = 'en', to_language: str = 'en') -> talkResult
+        A method that speaks a phrase
+
+    hear(srv: List) -> hearResult
+        A method that listens and verify the answer to a service
+
+    """
+
+    def __init__(self):
+        self.client_talk = actionlib.SimpleActionClient('/talk', talkAction)
+        self.client_hear = actionlib.SimpleActionClient('/hear', hearAction)
+
+        rospy.loginfo('Waiting for server: hear')
+        self.client_hear.wait_for_server()
+        rospy.loginfo('Waiting for server: talk')
+        self.client_talk.wait_for_server()
+
+        def talk(self, phrase: str, from_language: str = 'en', to_language: str = 'en') -> talkResult:
+            """
+            A method that speaks a phrase
+            """
+            goal = talkGoal(phrase=phrase, from_language=from_language, to_language=to_language)
+            self.client_talk.send_goal(goal)
+            self.client_talk.wait_for_result()
+            return self.client_talk.get_result()
+
+        def hear(self, srv: List) -> hearResult:
+            """
+            A method that listens to a service
+            """
+            goal = hearGoal(spec=srv.spec, choices=srv.choices)
+            self.client_hear.send_goal(goal)
+            self.client_hear.wait_for_result()
+            return self.client_hear.get_result()
