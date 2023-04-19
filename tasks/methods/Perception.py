eb743422229a6b2fa45756f2a4019f76bdc53cfd
khaledhazime <hazimekhaled01@gmail.com>
2023-04-19 02:16:16 -0300

    feat(Perception.py): add Perception class with methods to save object location and find objects

---
+++ tasks/methods/Perception.py
@@ -0,0 +1,129 @@
+import rospy
+import actionlib
+import sys
+import tf
+from tf import TransformListener
+from geometry_msgs.msg import Pose, PoseStamped
+
+from hera.msg import faceFeedback, faceResult, faceAction, faceGoal
+from hera_objectects.srv import Findobjectect, FindSpecificobjectect
+from hera_objectects.msg import *
+from hera_face.srv import *
+
+
+class Perception:
+    def __init__(self):
+        self.tf_listener = tf.TransformListener()
+
+        self.face_checking = rospy.ServiceProxy('/face_check', face_check)
+        self.save_face = rospy.ServiceProxy('/face_captures', face_capture)
+        self.recog_face = rospy.ServiceProxy('/face_recog', face_list)
+        self.objectects = rospy.ServiceProxy('/objectects', Findobjectect)
+        self.color_filter = rospy.ServiceProxy('/color_filter', color_detect)
+        self.specific_objectect = rospy.ServiceProxy('/specific_objectect', FindSpecificobjectect)
+
+        self.client_face = actionlib.SimpleActionClient('/face', faceAction)
+
+        rospy.loginfo('Waiting for server: objectects')
+        rospy.wait_for_service('/objectects')
+        rospy.loginfo('Waiting for server: specific_objectect')
+        rospy.wait_for_service('/specific_objectect')
+
+        rospy.loginfo('Waiting for server: face')
+        self.client_face.wait_for_server()
+
+    def save_obj_location(self, name, obj, dist):
+        for i in range(2):
+            listener = TransformListener()
+            rospy.sleep(5)
+            t = rospy.Time(0)
+
+            pose = Pose()
+            # pegando orientacao do robo
+            (robot_trans, robot_rot) = self.tf_listener.lookupTransform('/map', '/base', rospy.Time(0))
+            # Relacao Obj -> map (com a mesma orientacao do robo)
+            # --------------------------------------
+            pose.position.x = obj.position.x
+            pose.position.y = obj.position.y
+            pose.position.z = obj.position.z
+            pose.orientation.x = robot_rot[0]
+            pose.orientation.y = robot_rot[1]
+            pose.orientation.z = robot_rot[2]
+            pose.orientation.w = robot_rot[3]
+            self.save_obj_local("pose_1", pose)
+
+            pose_2 = Pose()
+            # tf Obj -> robo
+            (obj_trans, obj_rot) = self.tf_listener.lookupTransform('/base', '/pose_1', rospy.Time(0))
+            pose_2.position.x = obj_trans[0]
+            pose_2.position.y = obj_trans[1]
+            pose_2.position.z = obj_trans[2]
+            pose_2.orientation.x = obj_rot[0]
+            pose_2.orientation.y = obj_rot[1]
+            pose_2.orientation.z = obj_rot[2]
+            pose_2.orientation.w = obj_rot[3]
+
+            print("Pose2_antes:", pose_2)
+            # retirando a distancia
+            pose_2.position.x = pose_2.position.x - dist
+            pose_2.position.y = pose_2.position.y + 0.1
+
+            # --------------------------------------
+
+            print("Pose2_dps:", pose_2)
+            pose_stamped = PoseStamped()
+            pose_stamped.pose = pose_2
+            pose_stamped.header.frame_id = '/base'
+            pose_stamped.header.stamp = rospy.Time(0)
+
+            listener.waitForTransform("/map", "/base", t, rospy.Duration(5))
+
+            if listener.canTransform("/map", "/base", t):
+                pose_final = listener.transformPose("/map", pose_stamped)
+                print("Pose_final_stamped:", pose_final)
+
+            else:
+                rospy.logerr('Transformation is not possible!')
+                sys.exit(0)
+            # pose_final.position.x = transformed_pose[0]
+            # pose_final.position.y = transformed_pose[1]
+            # pose_final.position.z = transformed_pose[2]
+            # pose_final.orientation.x = transformed_rot[0]
+            # pose_final.orientation.y = transformed_rot[1]
+            # pose_final.orientation.z = transformed_rot[2]
+            # pose_final.orientation.w = transformed_rot[3]
+
+            # self.save_obj_local("pose_final", pose_final)
+            # -------------------------------------
+            ##Relacao pose_final -> map (com a mesma orientacao do robo)
+
+        pose_final_2 = Pose()
+        # (final_trans, final_rot) = self.tf_listener.lookupTransform('/map', "/pose_final", rospy.Time(0))
+        pose_final_2.position.x = pose_final.pose.position.x
+        pose_final_2.position.y = pose_final.pose.position.y
+        pose_final_2.position.z = pose_final.pose.position.z
+        pose_final_2.orientation.x = robot_rot[0]
+        pose_final_2.orientation.y = robot_rot[1]
+        pose_final_2.orientation.z = robot_rot[2]
+        pose_final_2.orientation.w = robot_rot[3]
+        print("Pose_final:", pose_final_2)
+
+        resp = self.save_obj_local(name, pose_final_2)
+        print("resp: ", resp)
+        return True  # if resp.result == 'SUCCEDED' else False
+
+    def find_obj(self, condition=""):
+        resp = self.objects(condition)
+        values = [resp.position.x, resp.position.y, resp.position.z]
+        if all(v == 0.0 for v in values):
+            return None
+        else:
+            return resp
+
+    def FindSpecificObject(self, tipo=""):
+        resp = self.specific_object(tipo)
+        values = [resp.position.x, resp.position.y, resp.position.z]
+        if all(v == 0.0 for v in values):
+            return None
+        else:
+            return resp
